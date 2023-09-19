658d2533d39f37b566bc4fae42d0330ac6c17a50
ylfyt <ayudi3521@gmail.com>
2023-09-19 19:13:27 +0700

    Create clipper component

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,28 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+	import Clipper from './components/clipper.svelte';
+
+	let youtubeTab: chrome.tabs.Tab | undefined;
+
+	onMount(async () => {
+		const activeTab = (await chrome.tabs.query({ active: true, currentWindow: true }))[0];
+		if (activeTab.url?.indexOf('youtube.com/watch') === -1) {
+			return;
+		}
+		const regex = /[?&]v=([^&#]+)/;
+		const match = activeTab.url?.match(regex);
+		const videoId = match && match[1];
+		if (!videoId) {
+			return;
+		}
+		youtubeTab = activeTab;
+	});
 </script>
 
 <main>
-	<h1>Hello</h1>
+	{#if !youtubeTab}
+		<h3>Not in a youtube video tab</h3>
+	{:else}
+		<Clipper tab={youtubeTab} />
+	{/if}
 </main>
---
+++ src/components/clipper.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	export let tab: chrome.tabs.Tab;
+</script>
+
+<div>
+	<h3 id="title">{tab.title}</h3>
+	<div style="margin-bottom: 10px">
+		<div style="display: flex; gap: 10px">
+			<div>
+				<div>Start</div>
+				<input type="text" id="start" placeholder="HH:MM:SS" />
+			</div>
+			<div>
+				<div>End</div>
+				<input type="text" id="end" placeholder="HH:MM:SS" />
+			</div>
+		</div>
+		<div style="display: flex">
+			<div>Loop:</div>
+			<input type="checkbox" id="loop" />
+		</div>
+	</div>
+	<button id="save-button">Save</button>
+	<button id="clear-button">Clear</button>
+</div>
