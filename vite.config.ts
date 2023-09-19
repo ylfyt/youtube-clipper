367373d87f5329530998abf73d513e51a6ec8b24
ylfyt <ayudi3521@gmail.com>
2023-09-19 21:23:56 +0700

    Adding entry point for content-script

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [svelte()],
+	build: {
+		outDir: './dist',
+		lib: {
+			entry: [resolve(__dirname, './index.html'), resolve(__dirname, './src/content-script.ts')],
+			name: '1',
+			formats: ['es'],
+			fileName: (format, entryName) => {
+				const extension = format === 'es' ? 'js' : 'cjs';
+				return `${entryName}.${extension}`;
+			},
+		},
+	},
 });
